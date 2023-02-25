574a30a6a00689999cd07a08c6e2e944a9a2c9aa
bogdanzilinets <114831468+bogdanzilinets@users.noreply.github.com>
2023-02-25 20:03:22 +0200

    task 2 done

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+// 1.Створить окремий елемент <li>. Обов'язково використовуй метод document.createElement().
+ingredients.forEach((item) => {
+  const addLiRef = document.createElement("li")
+
+  addLiRef.textContent = item;
+  addLiRef.className = "item";
+
+  document.querySelector("ul#ingredients").append(addLiRef)
+})
+
